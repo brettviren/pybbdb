a9f793a0c6e80088d52dd2d9b91ed727182b202a
Glenn Hutchings <zondo42@gmail.com>
2012-12-16 22:32:47 +0000

    Initial import.

---
+++ bbdb.py
@@ -0,0 +1,204 @@
+"""
+Interface to the Insidious Big Brother Database
+
+http://bbdb.sourceforge.net
+"""
+
+
+from pyparsing import (Regex, QuotedString, Keyword, Suppress, Word,
+                       Group, OneOrMore, ZeroOrMore, Or, nums, alphanums)
+
+
+class BBDB(list):
+    """
+    A BBDB database.
+    """
+
+    def __init__(self, path=None):
+        if path:
+            self.read(path)
+
+    def read(self, path):
+        for data in grammar.parseFile(path, parseAll=True):
+            self.add(*data)
+
+    def add(self, *args, **kw):
+        entry = Entry(*args, **kw)
+        self.append(entry)
+        return entry
+
+    def write(self, path):
+        pass
+
+
+class BBDBItem(dict):
+    def __getattr__(self, attr):
+        return self[attr]
+
+    def __setattr__(self, attr, value):
+        if attr in self:
+            self[attr] = value
+        else:
+            raise AttributeError("entry has no attribute '%s'" % attr)
+
+
+class Entry(BBDBItem):
+    """
+    A single BBDB entry.
+    """
+
+    def __init__(self, firstname="", lastname="", aka=[], company="",
+                 phone=[], address=[], net=[], notes=[], cache=None):
+        self["firstname"] = firstname
+        self["lastname"] = lastname
+        self["company"] = company
+
+        self["aka"] = []
+        if aka:
+            for name in aka:
+                self.add_aka(name)
+
+        self["phone"] = {}
+        if phone:
+            for tag, number in phone:
+                self.add_phone(tag, number)
+
+        self["address"] = {}
+        if address:
+            for args in address:
+                self.add_address(*args)
+
+        self["net"] = []
+        if net:
+            for name in net:
+                self.add_net(name)
+
+        self["notes"] = {}
+        if notes:
+            for tag, text in notes:
+                self.add_note(tag, text)
+
+        self["cache"] = cache
+
+    def set_name(self, firstname, lastname):
+        self["firstname"] = firstname
+        self["lastname"] = lastname
+
+    def add_aka(self, name):
+        self["aka"].append(name)
+
+    def add_phone(self, tag, number):
+        self["phone"][tag] = number
+
+    def add_address(self, tag, streets=[], city="", state="",
+                    zipcode="", country=""):
+        address = Address(streets, city, state, zipcode, country)
+        self["address"][tag] = address
+        return address
+
+    def add_net(self, name):
+        self["net"].append(name)
+
+    def add_note(self, tag, text):
+        self["notes"][tag] = text
+
+    @property
+    def name(self):
+        return (self.firstname + " " + self.lastname).strip()
+
+    def write(self, fp):
+        pass
+
+
+class Address(BBDBItem):
+    def __init__(self, streets=[], city="", state="", zipcode="", country=""):
+        self["streets"] = list(streets)
+        self["city"] = city
+        self["state"] = state
+        self["zipcode"] = zipcode
+        self["country"] = country
+
+    def add_street(self, street):
+        self["streets"].append(street)
+
+
+def make_grammar():
+    """
+    Construct the BBDB grammar.  See bbdb.ebnf for the specification.
+    """
+
+    # Define the low-level entities.
+    string = QuotedString(quoteChar='"', escChar='\\')
+    string.setParseAction(lambda tokens: tokens[0].strip())
+
+    nil = Keyword("nil")
+    nil.setParseAction(lambda tokens: [None])
+
+    atom = Word(alphanums + '-')
+    dot = Suppress(Keyword("."))
+
+    integer = Word(nums)
+    integer.setParseAction(lambda tokens: int(tokens[0]))
+
+    # Define helper functions for the brace types.
+    LP, RP, LB, RB = map(Suppress, "()[]")
+    Paren = lambda arg: LP + Group(arg) + RP
+    Bracket = lambda arg: LB + Group(arg) + RB
+
+    # Phone.
+    phone_usa = Group(OneOrMore(integer))
+    phone_nonusa = string
+    phone = Bracket(string + Or([phone_usa, phone_nonusa]))
+
+    # Address.
+    address_list = Paren(OneOrMore(string))
+    address = Bracket(string + address_list + string * 4)
+
+    # Note.
+    note = Paren(atom + dot + string)
+
+    # A single entry.
+    bbdb_entry = Bracket(string("firstname") + string("lastname") +
+                         Or([Paren(OneOrMore(string)), nil])("aka") +
+                         Or([string, nil])("company") +
+                         Or([Paren(OneOrMore(phone)), nil])("phone") +
+                         Or([Paren(OneOrMore(address)), nil])("address") +
+                         Or([Paren(OneOrMore(string)), nil])("net") +
+                         Or([Paren(OneOrMore(note)), nil])("notes") +
+                         nil("cache"))
+
+    # All the entries.
+    bbdb = ZeroOrMore(bbdb_entry)
+
+    # Define comment syntax.
+    comment = Regex(r";.*")
+    bbdb.ignore(comment)
+
+    return bbdb
+
+
+grammar = make_grammar()
+
+
+if __name__ == "__main__":
+    import os
+    path = os.path.expanduser("~/bbdb/bbdb.el")
+    bbdb = BBDB(path)
+
+    db = BBDB()
+    fred = db.add("Fred", "Flintstone")
+
+    fred.add_phone("Home", "555-1234")
+
+    fred.add_net("fred@bedrock.org")
+    fred.add_note("spouse", "Wilma")
+    fred.add_note("catchphrase", '"Yabba dabba doo!"')
+
+    fred.company = "Slate Rock & Gravel"
+
+    home = fred.add_address("Home")
+    home.add_street("345 Cavestone Road")
+    home.city = "Bedrock"
+
+    import json
+    print json.dumps(bbdb, sort_keys=False, indent=4, separators=(',', ': '))
