cb7aa18a017228fb79ea602e3deba3ae6bc94efd
Glenn Hutchings <810102+zondo@users.noreply.github.com>
2015-06-13 16:27:33 +0100

    Add bbdb_file() utility function.

--- bbdb/utils.py
+++ bbdb/utils.py
@@ -2,6 +2,9 @@
 Various utilities.
 """
 
+import os
+import subprocess
+
 from collections import OrderedDict
 from functools import total_ordering
 
@@ -24,5 +27,36 @@ class SortedDict(OrderedDict):
         self.update(sorted(items))
 
 
+def bbdb_file(user=None):
+    """
+    Return the standard BBDB file of the specified user.
+    If no user is given, default to the current user.
+
+    This is the file referred to by the 'bbdb-file' variable in emacs.
+    The most reliable way to get it is to ask emacs directly.
+    """
+
+    if not user:
+        user = os.environ["USER"]
+
+    tag = "BBDB="
+    cmd = "emacs --batch --user " + user
+    cmd += " --eval '(message \"%s%%s\" bbdb-file)' --kill" % tag
+
+    text = subprocess.check_output(cmd, shell=True,
+                                   stderr=subprocess.STDOUT)
+
+    for line in text.split("\n"):
+        if line.startswith(tag):
+            path = line.replace(tag, "").strip()
+            return os.path.expanduser(path)
+
+    return None
+
+
 def quote(string):
     return '"' + string.replace('"', r'\"') + '"'
+
+
+if __name__ == "__main__":
+    print bbdb_file()
