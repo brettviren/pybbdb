edfbf660b8279b727eb7a8d8453ea3c5bf27efd0
Brett Viren <brett.viren@gmail.com>
2018-01-27 15:59:57 -0500

    Initial support for parsing BBDB file-format 9

--- bbdb/parser.py
+++ bbdb/parser.py
@@ -10,10 +10,11 @@ from pyparsing import (Regex, QuotedString, Keyword, Suppress, Word,
 
 # File properties appearing in header comments.
 properties = (("coding", r"coding: (.+);", lambda s: s),
+              ("fileversion", r"file-format: (\d+)", lambda s: int(s)),
               ("fileversion", r"file-version: (\d+)", lambda s: int(s)))
 
 
-def parse(text):
+def parse(text, version=None):
     data = {}
 
     # Extract properties from comment lines.
@@ -24,14 +25,19 @@ def parse(text):
                 if m:
                     data[attr] = func(m.group(1))
 
+    print (data)
+    version = data.get("fileversion", version or 2)
+
     # Parse text for BBDB records.
-    results = grammar.parseString(text, parseAll=True)
+    gram = grammars[version]()
+    results = gram.parseString(text, parseAll=True)
     data["records"] = list(results)
 
     return data
 
 
-def make_grammar():
+
+def make_grammar_2():
     """
     Construct the BBDB grammar.  See grammar.ebnf for the specification.
     """
@@ -132,15 +138,135 @@ def make_grammar():
 
     return bbdb
 
+def make_grammar_9():
+    """
+    Construct the BBDB grammar Version 9.
+    """
 
-# Build the BBDB grammar.
-grammar = make_grammar()
+    # Helper functions for the brace types.
+    LP, RP, LB, RB = map(Suppress, "()[]")
+    Paren = lambda arg: LP + Group(arg) + RP
+    Bracket = lambda arg: LB + Group(arg) + RB
 
+    # Helper functions for constructing return types.
+    def make_list(t):
+        return t.asList()
+
+    def make_dict(t):
+        return {k: v for k, v in t[0] or []}
+
+    def make_address_entry(t):
+        return t[0].tag, {"location": list(t[0].location or []),
+                          "city": t[0].city or "",
+                          "state": t[0].state or "",
+                          "zipcode": t[0].zipcode or "",
+                          "country": t[0].country or ""}
+
+    def make_record(t):
+        return {"firstname": t[0].firstname,
+                "lastname": t[0].lastname,
+                "affix" : t[0].affix or [],
+                "aka": t[0].aka or [],
+                "company": t[0].company or "",
+                "phone": t[0].phone or {},
+                "address": t[0].address or {},
+                "net": t[0].net or [],
+                "fields": t[0].fields or {},
+                "uuid" : t[0].uuid or "",
+                "creation" : t[0].creation or "",
+                "timestamp" : t[0].timestamp or "",
+        }
+
+    def make_string(t):
+        return t[0][1:-1].replace(r'\"', '"')
+
+    # Define the low-level entities.
+    string = QuotedString(quoteChar='"', escChar='\\', unquoteResults=False)
+    string.setParseAction(make_string)
+
+    nil = Keyword("nil")
+    nil.setParseAction(lambda t: [None])
+
+    atom = Word(alphanums + '-')
+    dot = Suppress(Keyword("."))
+
+    integer = Word(nums)
+    integer.setParseAction(lambda t: int(t[0]))
+
+    # Phone.
+    phone_usa = Group(OneOrMore(integer))
+    phone_nonusa = string
+    phone_entry = Bracket(string("tag") + Or([phone_usa, phone_nonusa]))
+    phone = Or([Paren(OneOrMore(phone_entry)), nil])("phone")
+    phone.setParseAction(make_dict)
+
+    # Address.
+    location = Paren(OneOrMore(string))("location")
+    location.setParseAction(make_list)
+
+    address_entry = Bracket(string("tag") + location + string("city") +
+                            string("state") + string("zipcode") +
+                            string("country"))
+    address_entry.setParseAction(make_address_entry)
+    address = Or([Paren(OneOrMore(address_entry)), nil])("address")
+    address.setParseAction(make_dict)
+
+    # Field.
+    field = Paren(atom + dot + string)
+    fields = Or([Paren(OneOrMore(field)), nil])("fields")
+    fields.setParseAction(make_dict)
+
+    # Other parts of an entry.
+    name = string("firstname") + Or([string("lastname"), nil])
+    company = Or([string, nil])("company")
+
+    affix = Or([Paren(OneOrMore(string)), nil])("affix")
+    affix.setParseAction(make_list)
+
+    aka = Or([Paren(OneOrMore(string)), nil])("aka")
+    aka.setParseAction(make_list)
+
+    net = Or([Paren(OneOrMore(string)), nil])("net")
+    net.setParseAction(make_list)
+
+    uuid = string("uuid")
+    creation = string("creation")
+    timestamp = string("timestamp")
+
+    cache = nil("cache")
+
+    # A single record.
+    record = Bracket(name + affix + aka + company + phone + address + net +
+                     fields + uuid + creation + timestamp + cache)
+
+    record.setParseAction(make_record)
+
+    # All the records.
+    bbdb = ZeroOrMore(record)
+    bbdb.setParseAction(make_list)
+
+    # Define comment syntax.
+    comment = Regex(r";.*")
+    bbdb.ignore(comment)
+
+    return bbdb
+
+
+grammars = {2: make_grammar_2, 9: make_grammar_9}
 
 if __name__ == "__main__":
     import sys
     import json
 
-    text = open("parser.el").read()
-    data = parse(text)
-    json.dump(data, sys.stdout, indent=4)
+    for ver in sorted(grammars):
+        testfile = "parser%d.el" % ver
+        print ('Loading test file: "%s"' % testfile)
+        text = open(testfile).read()
+        print ("Parsing version %d" % ver)
+        data = parse(text)
+        json.dump(data, sys.stdout, indent=4)
+        if (ver == 9):
+            assert(data['fileversion'] == ver)
+            assert("uuid" in data["records"][0])
+
+        print("\n")
---
+++ bbdb/parser9.el
@@ -0,0 +1,44 @@
+;; -*-coding: utf-8-emacs;-*-
+;;; file-format: 9
+;;; user-fields: (fax mail-alias)
+[
+ ;; First and last names.
+ "Fred" "Flintstone"
+
+ ;; affix
+ ("Jr.")
+
+ ;; AKA.
+ ("Freddie")
+
+ ;; Company.
+ "Slate Rock & Gravel"
+
+ ;; Mapping of tag names to phone numbers.
+ (["Home" "555-1234"] ["Work" "555-6789"])
+
+ ;; Mapping of tag names to addresses.
+ (["Home"
+   ("Cave 2a" "345 Cavestone Road") "Bedrock" "Hanna Barbera" "12345" "USA"])
+
+ ;; List of email addresses.
+ ("fred@bedrock.org" "fred.flintstone@gravel.com")
+
+ ;; Alist of fields.
+ ((catchphrase . "\"Yabba dabba doo!\"")
+  (kids . "Pebbles, Bam-Bam")
+  (spouse . "Wilma"))
+
+ ;; UUID
+ "60c82ee1-2f7b-4220-809c-dbb25ab81239"
+
+ ;; creation
+ "2018-01-27 20:02:51 +0000"
+
+ ;; timestamp
+ "2018-01-27 20:02:51 +0000"
+
+ ;; Unused cache field.
+ nil
+]
+
